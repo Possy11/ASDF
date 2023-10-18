dc09ecc4ac7e0c4d6fe58a67c50464d59d844627
Possy_11 <148293736+Possy11@users.noreply.github.com>
2023-10-17 21:20:34 -0300

    primeiro commit

---
+++ frutas = ['maçã', 'pera', 'pessego'].py
@@ -0,0 +1,12 @@
+frutas = ['maçã', 'pera', 'pessego']
+frutas.insert(0, "Outra fruta")
+
+frutas.append('dado')
+frutas[4] = "Manga"
+
+print(frutas)
+posicao = 0
+while posicao < len(frutas):
+    frutas[posicao] = frutas[posicao] + "ss"
+    posicao+=1
+print(frutas)
